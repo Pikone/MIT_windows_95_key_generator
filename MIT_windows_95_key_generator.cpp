edba6a3be0f73dbe219eef29c6a4842607db7a0c
Pikksel One <47038939+Pikone@users.noreply.github.com>
2020-07-24 12:49:49 +0300

    Small changes

--- MIT_windows_95_key_generator.cpp
+++ MIT_windows_95_key_generator.cpp
@@ -7,40 +7,38 @@ int main (int argc, char *argv[])
 	srand(time(0));
 	int key_p1, key_p2, key_p3[7], key_p4[5];
 
-	int i, numOfKeys, valid;
+	int i, valid;
 
-    numOfKeys = atoi(argv[1]);
+	printf("%d keys\n", atoi(argv[1]));
 
-    printf("%d keys\n", numOfKeys);
-
-for(int u = 0; u < numOfKeys; u++)    
-{
-	key_p1 = rand() % 266 + 100;
-	key_p2 = rand() % 5 + 95;
-
-	do{
-		valid = 0;
-		for (i = 0; i < 7; i++)
-		{
-			key_p3[i] = rand() % 10;
-			valid += key_p3[i];
-		}
-	}while(valid % 7 != 0);
-
-	std::cout << key_p1 << key_p2 << "-OEM-0";
-
-	for (i = 0; i < 7; i++)
-		std::cout << key_p3[i];
-
-	std::cout << "-";
-
-	for (i = 0; i < 5; i++)
+	for(int u = 0; u < atoi(argv[1]); u++)
 	{
-		key_p4[i] = rand() % 10;
-		std::cout << key_p4[i];
+		key_p1 = rand() % 266 + 100;
+		key_p2 = rand() % 5 + 95;
+
+		do{
+			valid = 0;
+			for (i = 0; i < 7; i++)
+			{
+				key_p3[i] = rand() % 10;
+				valid += key_p3[i];
+			}
+		}while(valid % 7 != 0);
+
+		std::cout << key_p1 << key_p2 << "-OEM-0";
+
+		for (i = 0; i < 7; i++)
+			std::cout << key_p3[i];
+
+		std::cout << "-";
+
+		for (i = 0; i < 5; i++)
+		{
+			key_p4[i] = rand() % 10;
+			std::cout << key_p4[i];
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
-}
 
 	return 0;
 }
