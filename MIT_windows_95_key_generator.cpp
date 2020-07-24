268195f01f6fb6d632b61b4302fed3e18971fcc5
Pikksel One <47038939+Pikone@users.noreply.github.com>
2020-07-24 18:03:38 +0300

    Improved the generation algorithm

--- MIT_windows_95_key_generator.cpp
+++ MIT_windows_95_key_generator.cpp
@@ -5,16 +5,30 @@
 int main (int argc, char *argv[])
 {
 	srand(time(0));
-	int key_p1, key_p2, key_p3[7], key_p4[5];
+	int key_p1, key_p2[8] = {95, 96, 97, 98, 99, 01, 02, 03}, key_p3[7];
 
-	int i, valid;
+	int i, valid, random_control;
 
 	printf("%d keys\n", atoi(argv[1]));
+
+//Main algorithm
 
 	for(int u = 0; u < atoi(argv[1]); u++)
 	{
 		key_p1 = rand() % 366;
-		key_p2 = rand() % 5 + 95;
+
+		if (key_p1 < 100){
+			std::cout << 0;
+			if (key_p1 < 10){
+				std::cout << 0;
+			}
+		}
+		std::cout << key_p1;
+
+		random_control = rand() % 7;
+		if (key_p2[random_control] < 4)
+			std::cout << 0;
+		std::cout << key_p2[random_control] << "-OEM-0";
 
 		do{
 			valid = 0;
@@ -25,27 +39,13 @@ int main (int argc, char *argv[])
 			}
 		}while(valid % 7 != 0);
 
-		if (key_p1 < 100)
-		{
-			std::cout << 0;
-			if (key_p1 < 10)
-			{
-				std::cout << 0;
-			}
-		}
-
-		std::cout << key_p1 << key_p2 << "-OEM-0";
-
 		for (i = 0; i < 7; i++)
 			std::cout << key_p3[i];
 
 		std::cout << "-";
 
 		for (i = 0; i < 5; i++)
-		{
-			key_p4[i] = rand() % 10;
-			std::cout << key_p4[i];
-		}
+			std::cout << rand() % 10;
 		std::cout << std::endl;
 	}
 
